4c0c2802940c1cd91ccce585f82303b966016785
JayateerthDambal <jayateerth.dambal@gmail.com>
2023-02-19 22:37:08 +0530

    Completed OAuth Process using Flask as a Backend Server

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,67 @@
-from flask import Flask
+import json
+import os
+import secrets
+import string
+
 import stripe
-from flask import request
+from dotenv import load_dotenv, find_dotenv
+from flask import Flask, jsonify, render_template, redirect, request, session, send_from_directory
+import urllib
+
+# Setup Stripe python client library
+load_dotenv(find_dotenv())
+stripe.api_key = os.getenv('STRIPE_SECRET_KEY')
+stripe.api_version = os.getenv('STRIPE_API_VERSION', '2019-12-03')
+
+static_dir = str(os.path.abspath(os.path.join(__file__ , "..", os.getenv("STATIC_DIR"))))
+app = Flask(__name__, static_folder=static_dir,
+            static_url_path="", template_folder=static_dir)
+
+# Set the secret key to some random bytes. Keep this really secret!
+# This enables Flask sessions.
+app.secret_key = b'_5#y2L"JF878z\n\xec]/'
+
+@app.route("/get-oauth-link/", methods=["GET"])
+def construct_oauth_link():
+    # state = ''.join([secrets.choice(string.ascii_letters + string.digits) for n in range(16)])
+    get_data = request.args.to_dict()
+    
+    state = get_data['state']
+    session['state'] = state
+    args = {
+      "client_id": os.getenv('STRIPE_CLIENT_ID'),
+      "state": state,
+      "scope": "read_write",
+      "response_type": "code",  
+    }
+    url = "https://connect.stripe.com/oauth/authorize?{}".format(urllib.parse.urlencode(args))
+    print("URL: ", url)
+    return redirect(url)
 
 
-stripe.api_key = ""
-app = Flask(__name__)
+@app.route("/authorize-oauth", methods=["GET"])
+def handle_oauth_redirect():
+  if request.args.get("state") != session['state']:
+    return json.dumps({"error": "Incorrect state parameter: " + request.args.get("state")}), 403
 
+  # Send the authorization code to Stripe's API.
+  code = request.args.get("code")
+  try:
+    response = stripe.OAuth.token(grant_type="authorization_code", code=code,)
+  except stripe.oauth_error.OAuthError as e:
+    return json.dumps({"error": "Invalid authorization code: " + code}), 400
+  except Exception as e:
+    return json.dumps({"error": "An unknown error occurred."}), 500
 
-@app.route('/oauth/login/', methods=['GET', 'POST'])
-def auth_login():
-    args = request.args.to_dict()
-    print(args)
-    return args
+  connected_account_id = response["stripe_user_id"]
+  save_account_id(connected_account_id)
 
+  
+  return redirect("https://dashboard.stripe.com/test/dashboard")
+
+def save_account_id(id):
+  # Save the connected account ID from the response to your database.
+  print("Connected account ID: ", id)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- frontend/src/views/App.tsx
+++ frontend/src/views/App.tsx
@@ -12,23 +12,31 @@ const clientID = 'ca_NMVdZWTZSaRhb4nrYRxpPwJ3un4nMSqI';
 const getRedirectURL = (mode: 'live' | 'test') => `https://dashboard.stripe.com/${
   mode === 'test' ? 'test/' : ''}apps-oauth/com.example.oauth-example`;
 const getAuthURL = (state: string, challenge: string, mode: 'live' | 'test') =>
-  `http://localhost:5000/oauth/login/?response_type=code&client_id=${clientID}&redirect_uri=${getRedirectURL(mode)}&state=${state}&code_challenge=${challenge}&code_challenge_method=S256`;
+  `http://localhost:5000/get-oauth-link/?response_type=code&client&redirect&state=${state}&code_challenge=${challenge}&code_challenge_method=S256`;
 
   interface TokenData{
     account_id: string;
     access_token: string;
     expires_in: number;
   }
+  
 
 
-  const OAuthApp = ({environment}: ExtensionContextValue) => {
+  const OAuthApp = ({environment, oauthContext}: ExtensionContextValue) => {
   const {mode} = environment;
   const [authURL, setAuthURL] = useState('');
+  
+  const [tokenData, setTokenData] = useState<TokenData | null>(null);
+  const code = oauthContext?.code || '';
+  const verifier = oauthContext?.verifier || '';
+
+  // 1. Getting OAuth URL
   useEffect(() => {
     createOAuthState().then(({state, challenge}) => {
       setAuthURL(getAuthURL(state, challenge, mode));
     });
   }, [mode]);
+
 
   return (
     <ContextView title="OAuth">
@@ -38,25 +46,6 @@ const getAuthURL = (state: string, challenge: string, mode: 'live' | 'test') =>
 };
 export default OAuthApp;
 
-const getTokenFromAuthServer = async ({code, verifier, mode}: {code: string, verifier: string, mode: 'live' | 'test'}): Promise<null | TokenData> => {
-  try {
-    const response = await fetch(`https://api.example.com/oauth2/token?code=${code}&grant_type=authorization_code&code_verifier=${verifier}&client_id=${clientID}&redirect_uri=${getRedirectURL(mode)}`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-    });
-    if (response.ok) {
-      return await response.json();
-    }
-    throw new Error(await response.text());
-  } catch (e) {
-    console.error('Unable to retrieve token from authorization server:', (e as Error).message);
-    return null;
-  }
-};
 
-const RetrieveAccessToken = ({environment, oauthContext}: ExtensionContextValue)=> {
-  const [tokenData, setTokenData] = useState<TokenData | null>(null);
-  const code = oauthContext?.code || '';
-  const verifier = oauthContext?.verifier || '';
-  const {mode} = environment;
-}
+
+
