20ff124de0ac2c70c57a58c6723211460fc9ec8d
JayeerthDambal <jayateerth.dambal@gmail.com>
2023-04-09 18:43:30 +0530

    Changing the retrieve_current_payout function

--- backend/retrieve_current_payouts.py
+++ backend/retrieve_current_payouts.py
@@ -2,38 +2,43 @@ import stripe
 import numpy as np
 import pandas as pd
 import datetime
+import time
 
 
 def retrieve_current_payouts(api_key, current_month='', current_year=''):
     stripe.api_key = api_key
-    arrival_dates = [
-        datetime.datetime.fromtimestamp(stripe.Payout.list(limit=100)['data'][i]['arrival_date']).strftime("%m/%d/%Y")
-        for i in range(len(stripe.Payout.list(limit=100)['data']))]
-    arrival_dates_month_year = list(map(lambda x: x[:2] + ' ' + x[-4:], arrival_dates))
-    current_pay_out_ids = np.where(np.array(arrival_dates_month_year) == current_month + ' ' + current_year)
-    current_pay_out_ids = [stripe.Payout.list(limit=100)['data'][i]['id'] for i in current_pay_out_ids[0]]
+    dt = datetime.datetime(int(current_year), int(current_month), 1)
+    epoch_time = int(time.mktime(dt.timetuple()))
+    new_epoch_time = dt + datetime.timedelta(days=30)
+    new_epoch_time = int(new_epoch_time.timestamp())
 
-    output_df = pd.DataFrame([])
-    for pay_out_id in current_pay_out_ids:
-        transactions = stripe.BalanceTransaction.list(payout=pay_out_id, limit=100)
+    data_2 = stripe.Payout.list(arrival_date={
+        'gte': f'{epoch_time}',
+        'lt': f'{new_epoch_time}'
+    })
+    vals = []
+    result = pd.DataFrame()
+    for i in range(0, len(data_2)):
+        vals.append(data_2.data[i]["id"])
+
+    for val in vals:
+        transactions = stripe.BalanceTransaction.list(payout=val, limit=100)
         payout_total = transactions['data'][0]['amount'] / 100 * (-1)
+        for j in range(1, len(transactions['data'])):
+            created = datetime.datetime.fromtimestamp(transactions['data'][j]['created'])
+            description = transactions['data'][j]['description']
+            amount = transactions['data'][j]['amount'] / 100
+            currency = transactions['data'][j]['currency']
+            converted_amount = transactions['data'][j]['amount'] / 100
+            fees = transactions['data'][j]['fee'] / 100
+            net = transactions['data'][j]['net'] / 100
 
-        for i in range(1, len(transactions['data'])):
-            created = datetime.datetime.fromtimestamp(transactions['data'][i]['created'])
-            description = transactions['data'][i]['description']
-            amount = transactions['data'][i]['amount'] / 100
-            currency = transactions['data'][i]['currency']
-            converted_amount = transactions['data'][i]['amount'] / 100
-            fees = transactions['data'][i]['fee'] / 100
-            net = transactions['data'][i]['net'] / 100
-
-            output_df = pd.concat([output_df, pd.DataFrame([pay_out_id, payout_total, created,
+            result = pd.concat([result, pd.DataFrame([val, payout_total, created,
                                                             description, amount, currency,
                                                             converted_amount, fees,
                                                             net]).T], axis=0)
+    if result.shape[0] != 0:
+        result.columns = ['payout_id', 'payout_total', 'Created', 'Description', 'Amount', 'Currency',
+                                'Converted Amount', 'Fees', 'Net']
+    return result
 
-    if output_df.shape[0] != 0:
-        output_df.columns = ['payout_id', 'payout_total', 'Created', 'Description', 'Amount', 'Currency',
-                             'Converted Amount', 'Fees', 'Net']
-
-    return output_df
